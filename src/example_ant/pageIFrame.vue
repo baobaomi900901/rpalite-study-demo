261d3f26299a2185fb8f88bf5dd63d3d3035203a
林嘉伦 <linjialun@kingsware.cn>
2025-12-17 17:36:11 +0800

    fix: 修改iframe路径

--- src/example_ant/pageIFrame.vue
+++ src/example_ant/pageIFrame.vue
@@ -1,9 +1,11 @@
 <script setup lang='ts'>
-
+const src = process.env.NODE_ENV === 'production'
+  ? 'https://www.kingsware.cn/krpalite/tutorial-demo/iframe.html'
+  : '/iframe.html';
 </script>
 
 <template>
-  <iframe class="w-full h-full" src="/iframe.html"></iframe>
+  <iframe class="w-full h-full" :src="src"></iframe>
 </template>
 
 <style lang="less" scoped>
