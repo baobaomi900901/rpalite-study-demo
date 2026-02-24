09c6d6bbf8b5f9c253165f3991e5b5856dd6cff0
moby <baobaomi900901@icloud.com>
2026-02-24 15:49:37 +0800

    修改表单干扰项

--- src/example_ant/pageForm.vue
+++ src/example_ant/pageForm.vue
@@ -6,69 +6,99 @@
 * @version V3.0.0
 !-->
 <template>
-  <div id="pageForm" class="pageForm flex-1 px-20 py-10" style="min-width: 0; max-width: 50%">
-    <span>发票录入</span>
-    <br />
-    <br />
-    <!-- 干扰dom -->
-    <k-form
-      v-if="formData.enableInterference"
-      style="display: none;"
-      ref="myForm"
-      :model="formData"
-      :rules="rules"
-      label-position="left"
-      label-width="auto"
-      class="w-full"
-      showColon
-      :key="key"
-    >
-      <k-form-item label="发票代码">
-        <k-input></k-input>
-      </k-form-item>
-    </k-form>
-
-    <k-form
-      ref="myForm"
-      :model="formData"
-      :rules="rules"
-      label-position="left"
-      label-width="auto"
-      class="w-full"
-      showColon
-      :key="key"
-    >
-      <template v-for="(item, index) in contentList" :key="index">
-        <k-form-item v-if="item.type === 'input'" :label="item.label" :prop="item.prop">
-          <k-input v-model="formData[item.prop]"></k-input>
+  <div class="flex h-full">
+    <div id="pageForm" class="pageForm flex-1 px-20 py-10" style="min-width: 0; max-width: 50%">
+      <span>发票录入</span>
+      <br />
+      <br />
+      <!-- 干扰dom -->
+      <k-form
+        v-if="formData.enableInterference"
+        style="display: none"
+        ref="myForm"
+        :model="formData"
+        :rules="rules"
+        label-position="left"
+        label-width="auto"
+        class="w-full"
+        showColon
+        :key="key"
+      >
+        <k-form-item label="发票代码">
+          <k-input></k-input>
         </k-form-item>
-        <div class="flex gap-4">
-        <template v-if="item.type === 'input-img'">
-          <k-form-item :label="item.label" :prop="item.prop">
+      </k-form>
+
+      <k-form
+        ref="myForm"
+        :model="formData"
+        :rules="rules"
+        label-position="left"
+        label-width="auto"
+        class="w-full"
+        showColon
+        :key="key"
+      >
+        <template v-for="(item, index) in contentList" :key="index">
+          <k-form-item v-if="item.type === 'input'" :label="item.label" :prop="item.prop">
             <k-input v-model="formData[item.prop]"></k-input>
           </k-form-item>
-          <img src="../assets/verification.png" alt="code" class="h-8" />
+          <div class="flex gap-4">
+            <template v-if="item.type === 'input-img'">
+              <k-form-item :label="item.label" :prop="item.prop">
+                <k-input v-model="formData[item.prop]"></k-input>
+              </k-form-item>
+              <img src="../assets/verification.png" alt="code" class="h-8" />
+            </template>
+          </div>
         </template>
-      </div>
-      </template>
 
-      <k-form-item>
-        <div class="flex gap-4 w-full border-t pt-4 flex items-center">
-          <k-button main @click="submit(myForm)">提交</k-button>
-          <k-button secondary @click="resetForm(myForm)">重置</k-button>
-          <k-switch class="!ml-auto" v-model="formData.enableInterference" active-text="干扰项状态开关" />
-          <k-button main @click="rearrangement">重排</k-button>
-        </div>
-      </k-form-item>
-    </k-form>
+        <k-form-item>
+          <div
+            class="flex w-full border-t pt-4 flex items-center"
+            :style="{ paddingTop: moveButton ? '1000px' : undefined }"
+          >
+            <k-button main @click="submit(myForm)">提交</k-button>
+            <k-button secondary @click="resetForm(myForm)">重置</k-button>
+          </div>
+        </k-form-item>
+      </k-form>
+    </div>
+    <div class="border-t pt-4 flex flex-col gap-4">
+      <span class="pb-4">干扰项配置</span>
+      <k-switch v-model="formData.enableInterference" active-text="添加干扰项" />
+      <k-switch v-model="moveButton" active-text="移动提交按钮至屏幕外" />
+      <div class="flex">
+        <k-button pain @click="rearrangement">打乱顺序</k-button>
+        <k-button pain @click="dialogVisible = true">全屏遮罩</k-button>
+      </div>
+    </div>
   </div>
+  <k-dialog
+    v-model="dialogVisible"
+    title="Tips"
+    width="500"
+    :before-close="handleClose"
+    :close-on-click-modal="false"
+  >
+    <span>This is a message</span>
+    <template #footer>
+      <div class="dialog-footer">
+        <k-button @click="dialogVisible = false">Cancel</k-button>
+        <k-button type="primary" main @click="dialogVisible = false">Confirm</k-button>
+      </div>
+    </template>
+  </k-dialog>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
 import type { FormInstance } from 'element-plus';
+import { KMessageBox } from '@ksware/ksw-ux';
 
-const key = ref(0)
+const key = ref(0);
+
+const moveButton = ref(false);
 
 type FormKey = 'code' | 'number' | 'date' | 'verification' | 'total' | 'tax';
 
@@ -82,34 +112,34 @@ const contentList = ref<ContentItem[]>([
   {
     label: '发票代码',
     prop: 'code',
-    type: 'input'
+    type: 'input',
   },
   {
     label: '发票号码',
     prop: 'number',
-    type: 'input'
+    type: 'input',
   },
   {
     label: '开票日期',
     prop: 'date',
-    type: 'input'
+    type: 'input',
   },
   {
     label: '校验码',
     prop: 'verification',
-    type: 'input-img'
+    type: 'input-img',
   },
   {
     label: '合计金额',
     prop: 'total',
-    type: 'input'
+    type: 'input',
   },
   {
     label: '合计税额',
     prop: 'tax',
-    type: 'input'
+    type: 'input',
   },
-])
+]);
 
 const myForm = ref<FormInstance>();
 const formData = ref({
@@ -119,7 +149,7 @@ const formData = ref({
   verification: '',
   total: '',
   tax: '',
-  enableInterference: false
+  enableInterference: false,
 });
 
 const rules = {
@@ -146,10 +176,22 @@ function rearrangement() {
   contentList.value.sort(() => Math.random() - 0.5);
   key.value++;
 }
+
+const dialogVisible = ref(false);
+
+const handleClose = (done: () => void) => {
+  KMessageBox.confirm('Are you sure to close this dialog?')
+    .then(() => {
+      done();
+    })
+    .catch(() => {
+      // catch error
+    });
+};
 </script>
 <style lang="less" scoped>
 .el-button.k-button + .el-button.k-button {
-  margin-left: 0;
+  // margin-left: 0;
 }
 
 .k-tree-table .k-tree-table__header {
